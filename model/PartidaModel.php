2ca6611b9076883aca21fab8b2f632df219019d9
EricNahuel2002 <ericaquino2002@gmail.com>
2024-10-23 21:58:31 -0300

    propuse una idea de como podria ser el juego

--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -11,7 +11,7 @@ class PartidaModel
 
     public function getPregunta(){
         $idPregunta = random_int(1,1);
-        $query = "SELECT p.descripcion as pregunta, r.descripcion as respuesta, r.id as idRta
+        $query = "SELECT p.id as idPregunta, p.descripcion as pregunta, r.descripcion as respuesta, r.id as idRta
         FROM pregunta p join respuesta r on p.id = r.idPregunta WHERE p.id = ?";
         $stmt = $this->db->connection->prepare($query);
         $stmt->bind_param("i", $idPregunta);
@@ -19,11 +19,15 @@ class PartidaModel
         $result = $stmt->get_result();
 
         $data = [
+            "idPregunta" => "",
             "pregunta" => "",
             "respuestas" => []
         ];
 
         while ($row = $result->fetch_assoc()) {
+            if (empty($data["idPregunta"])){
+                $data["idPregunta"] = $row["idPregunta"];
+            }
             if (empty($data["pregunta"])) {
                 $data["pregunta"] = $row["pregunta"];
             }
