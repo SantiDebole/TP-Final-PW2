10bb956eea3385205f6718aa2acb1b909772cde5
Martin <ursomartin89@gmail.com>
2024-11-09 15:00:01 -0300

    modificaciones de partida

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -23,115 +23,91 @@ class PartidaController
             }
         }
     }
-    public function jugarNuevaPartida(){
+
+    public function jugarNuevaPartida()
+    {
         if (isset($_SESSION["user_id"])) {
-        $idUsuario = $_SESSION["user_id"];
-        $this->model->terminarPartida($idUsuario);
+            $idUsuario = $_SESSION["user_id"];
+
+            $this->model->terminarPartida($idUsuario);
             header("Location: /partida/crearPartida ");
         }
     }
 
 
-    public function crearPartida(){
+    public function crearPartida()
+    {
         if (isset($_SESSION["user_id"])) {
             $idUsuario = $_SESSION["user_id"];
             if ($this->model->consultarPartidaDisponible($idUsuario)) {
                 $this->presenter->show("reanudarPartida");
             } else {
-                $_SESSION['partida']= $this->model->crearPartida($idUsuario);
-                $preguntaConRespuestas = $this->model->getPreguntaConRespuestas($idUsuario);
+                $_SESSION['partida'] = $this->model->crearPartida($idUsuario);
+                header("Location: /partida/preguntar ");
+            }
+        }
+
+
+    }
+
+    public function preguntar()
+    {
+        $data = [];
+        if (isset($_SESSION["user_id"]) && isset($_SESSION['partida'])) {
+            $idUsuario = $_SESSION["user_id"];
+            $idPartida = $_SESSION['partida'];
+
+            $preguntaConRespuestas = $this->model->getPreguntaConRespuestas($idUsuario, $idPartida);
+            $puntaje = $this->model->traerPuntajeDelUsuarioEnLaPartida($idPartida, $idUsuario);
+            if ($preguntaConRespuestas) {
                 $data = [
+
                     "loggedUserId" => $idUsuario,
                     "pregunta" => $preguntaConRespuestas,
-                    "puntaje" => "0"
+                    "puntaje" => "$puntaje"
                 ];
-                $this->presenter->show("pregunta", $data);
+            $this->presenter->show("pregunta", $data);
             }
-        }
+        }else $this->presenter->show("partidaNoDisponible");
+    }
+
+    public function reanudarPartida()
+    {
+//cargo datos de partida
 
     }
-    public function reanudarPartida(){
 
-
-}
-
-
-}
-
-
-/*
-        if(isset($_SESSION["user_id"])){
+    public function responder()
+    {
+        if (isset($_SESSION["user_id"]) && isset($_SESSION['partida'])) {
             $idUsuario = $_SESSION["user_id"];
-            $pregunta = $this->model->jugar($idUsuario);
-            if($pregunta){
-                if(!isset($_SESSION["idPartida"])){
-                    $this->crearPartida($idUsuario);
+            $idPartida = $_SESSION['partida'];
+            if (isset($_POST["idRespuesta"])) {
+                $idRespuesta = $_POST["idRespuesta"];
+                if ($this->model->validarRespuesta($idRespuesta, $idPartida)) {
+                    $puntaje = $this->model->traerPuntajeDelUsuarioEnLaPartida($idPartida, $idUsuario);
+                    $data = [
+                        "loggedUserId" => $idUsuario,
+                        "resultado" => "Acerto!",
+                        "puntaje" => "$puntaje"
+                    ];
+                    $this->presenter->show("resultadoRespuesta", $data);
+                } else {
+                    $puntaje = $this->model->traerPuntajeDelUsuarioEnLaPartida($idPartida, $idUsuario);
+                    $data = [
+                        "loggedUserId" => $idUsuario,
+
+                        "puntaje" => "$puntaje"
+                    ];
+                    unset($_SESSION['partida']);
+                    $this->presenter->show("resultadoRespuesta", $data);
+
                 }
-                $idPartida = $_SESSION["idPartida"];
-                $puntaje= $this->model->traerPuntajeDelUsuarioEnLaPartida($idPartida,$idUsuario);
-                $data = [
-                    "loggedUserId" => $idUsuario,
-                    "pregunta" => $pregunta,
-                    "puntaje" => $puntaje
-                ];
-                $this->presenter->show("pregunta", $data);
-            }else{
-                $this->model->reiniciarRegistroDePreguntasVistasPorUsuario($idUsuario);
-                header("Location: /partida/jugar");
-                exit();
             }
 
-        }
+
+        }else $this->presenter->show("partidaNoDisponible");
+
     }
 
-
-    public function responder(){
-        $idRespuesta = $_POST["idRespuesta"];
-        $idPregunta = $_POST["idPregunta"];
-        $idUsuario = $_SESSION["user_id"];
-        $acierta = $this->model->validarRespuesta($idRespuesta);
-        if($acierta){
-            $idPartida = $_SESSION["idPartida"];
-            $this->model->registrarPreguntaCorrecta($idPartida, $idPregunta);
-            $this->model->marcarPreguntaVistaPorUsuario($idUsuario, $idPregunta);
-
-            header("location: /partida/jugar");
-            exit();
-        }else{
-            $idPartida = $_SESSION["idPartida"];
-            $this->model->registrarPreguntaIncorrecta($idPartida, $idPregunta);
-            $this->model->marcarPreguntaVistaPorUsuario($idUsuario, $idPregunta);
-            $this->model->desactivarPartida($idPartida);
-
-
-            // se destruye la variable de la sesion que contiene el id de la partida actual al finalizar la partida.
-
-            header("location: /partida/mostrarPuntos");
-            exit();
-        }
-    }
-
-    public function mostrarPuntos(){
-        //generaria la vista donde muestra los puntos obtenidos
-        $idUsuario = $_SESSION["user_id"];
-        $idPartida = $_SESSION["idPartida"];
-        $puntaje= $this->model->traerPuntajeDelUsuarioEnLaPartida($idPartida,$idUsuario);
-        $data = [
-            "loggedUserId" => $idUsuario,
-            "puntaje" => $puntaje
-        ];
-        unset($_SESSION["idPartida"]);
-        $this->presenter->show("mostrarPuntos",$data);
-    }
-
-    private function crearPartida($idUsuario){
-        $this->model->crearPartida($idUsuario);
-        $partida = $this->model->getPartida($idUsuario);
-        $_SESSION["idPartida"] = $partida["id"];
-    }
-
-
-
-
-
-}*/
+}
--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -8,22 +8,40 @@ class PartidaModel
     {
         $this->db = $db;
     }
+    public function validarRespuesta($idRespuesta,$idPartida){
+       $pregunta = $this->tienePreguntaDisponible($idPartida);
+       if($this->controladorDeExpiracionDePregunta($pregunta['fecha'])&&$this->controlarSiEsRespuestaCorrectaParaPreguntaObtenida($pregunta,$idRespuesta)){
+       $this->marcarPreguntaComoCorrecta($pregunta);
+        return true;}
+        else { $this->marcarPreguntaComoIncorrecta($pregunta);
 
-
-    public function getPreguntaConRespuestas($idUsuario){
-        $nivel = $this->calcularNivelDelUsuario($idUsuario);
-        $preguntasNoVistasPorUsuario = $this->getPreguntasNoVistasPorUsuario($idUsuario,$nivel);
-        if($preguntasNoVistasPorUsuario){
-            $idPregunta = $this->getPreguntaAleatoria($preguntasNoVistasPorUsuario);
-
-            return $this->obtenerRespuestasALaPregunta($idPregunta);
-        }else{
-            $this->reiniciarRegistroDePreguntasVistasPorUsuario($idUsuario);
-            $this->getPreguntaConRespuestas($idUsuario);
         }
+        $this->marcarPartidaComoFinalizada($idPartida);
+        return false;
 
-    }
+}
+    public function getPreguntaConRespuestas($idUsuario, $idPartida)
+    {
+        if ($this->tienePartidaDisponible($idUsuario)) {
 
+            $pregunta = $this->tienePreguntaDisponible($idPartida);
+            if ($pregunta) return $this->obtenerRespuestasALaPregunta($pregunta['idPregunta']);
+
+
+            $nivel = $this->calcularNivelDelUsuario($idUsuario);
+            $preguntasNoVistasPorUsuario = $this->getPreguntasNoVistasPorUsuario($idUsuario, $nivel);
+            if ($preguntasNoVistasPorUsuario) {
+                $idPregunta = $this->getPreguntaAleatoria($preguntasNoVistasPorUsuario);
+                $this->registrarPreguntaEnTienen($idPartida, $idPregunta);
+                $this->registrarPreguntaEnUsuarioPregunta($idUsuario, $idPregunta);
+
+                return $this->obtenerRespuestasALaPregunta($idPregunta);
+            } else {
+
+                $this->reiniciarRegistroDePreguntasVistasPorUsuario($idUsuario);
+              //  $this->getPreguntaConRespuestas($idUsuario, $idPartida);
+            }
+        }}
     public function crearPartida($idUsuario)
     {
 
@@ -37,7 +55,6 @@ class PartidaModel
 
 
     }
-
     public function consultarPartidaDisponible($idUsuario)
     {
         var_dump($idUsuario);
@@ -49,7 +66,7 @@ class PartidaModel
                     return true;
                 } else {
                     $this->marcarPreguntaComoIncorrecta($pregunta);
-                    $this->marcarPartidaComoFinalizada($partidaDisponible);
+                    $this->marcarPartidaComoFinalizada($partidaDisponible['id']);
                     return false;
                 }
             } else return true;
@@ -57,26 +74,25 @@ class PartidaModel
         return false;
 
     }
-
     public function terminarPartida($idUsuario)
     {
         $partidaDisponible = $this->tienePartidaDisponible($idUsuario);
+        var_dump($partidaDisponible['id']);
         $pregunta = $this->tienePreguntaDisponible($partidaDisponible['id']);
+        $this->marcarPartidaComoFinalizada($partidaDisponible['id']);
         $this->marcarPreguntaComoIncorrecta($pregunta);
-        $this->marcarPartidaComoFinalizada($partidaDisponible);
-    }
 
+    }
     private function marcarPartidaComoFinalizada($partidaDisponible)
     {
         $query = "update partida
         set estado = 'inactivo'
         where id = ?";
         $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("i", $partidaDisponible['id']);
+        $stmt->bind_param("i", $partidaDisponible);
         $stmt->execute();
 
     }
-
     private function marcarPreguntaComoIncorrecta($pregunta)
     {
         $query = "update tienen
@@ -86,17 +102,15 @@ class PartidaModel
         $stmt->bind_param("iis", $pregunta['idPartida'], $pregunta['idPregunta'], $pregunta['fecha']);
         $stmt->execute();
     }
-
     private function controladorDeExpiracionDePregunta($horario)
     {
+
         $fechaPregunta = new DateTime($horario);
         $fechaActual = new DateTime();
-        // var_dump($fechaActual);
         $intervalo = $fechaPregunta->diff($fechaActual); //Diff devuelve un DateInterval
         if ($intervalo->i < 1 && $intervalo->h == 0 && $intervalo->d == 0 && $intervalo->m == 0 && $intervalo->y == 0) return true;
         return false;
     }
-
     private function tienePartidaDisponible($idUsuario)
     {
         $query = "SELECT id 
@@ -109,9 +123,8 @@ class PartidaModel
         $result = $stmt->get_result();
         return $result->fetch_assoc();
 
-        return $resultado;
-    }
 
+    }
     private function tienePreguntaDisponible($idPartida)
     {
         $query = "SELECT idPartida, idPregunta, fecha
@@ -124,7 +137,6 @@ class PartidaModel
         $result = $stmt->get_result();
         return $result->fetch_assoc();
     }
-
     private function filtrarPreguntasPorNivelDeUsuario($nivel): string
     {
         $dificultad = "";
@@ -177,12 +189,13 @@ class PartidaModel
             return "alto";
         }
     }
-
-    public function marcarPreguntaVistaPorUsuario($idUsuario, $idPregunta){
-        $query = "INSERT INTO UsuarioPregunta (idUsuario, idPregunta) VALUES (?,?)";
-        $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("ii", $idUsuario, $idPregunta);
-        $stmt->execute();
+    private function marcarPreguntaComoCorrecta($pregunta){
+        $query="update tienen
+        set puntaje = 1
+      where idPartida = ? and idPregunta = ? and fecha = ?";
+    $stmt = $this->db->connection->prepare($query);
+    $stmt->bind_param("iis", $pregunta['idPartida'],$pregunta['idPregunta'],$pregunta['fecha']);
+    $stmt->execute();
     }
     public function reiniciarRegistroDePreguntasVistasPorUsuario($idUsuario){
         $query = "DELETE FROM UsuarioPregunta WHERE idUsuario = ?";
@@ -256,7 +269,31 @@ class PartidaModel
         //shuffle($data['respuestas']);
         return $data;
     }
-    /*
+    private function controlarSiEsRespuestaCorrectaParaPreguntaObtenida($pregunta,$idRespuesta){
+    var_dump($pregunta['idPregunta']);
+        $query="SELECT 1 
+FROM pregunta p
+JOIN respuesta r ON p.id = r.idPregunta
+WHERE r.esCorrecta = 1 AND r.id = ? and p.id=  ?
+;";
+    $stmt = $this->db->connection->prepare($query);
+    $stmt->bind_param("ii",$idRespuesta, $pregunta['idPregunta']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) return true;
+        return false;
+}
+    private function registrarPreguntaEnTienen($idPartida, $idPregunta){
+                $fecha = date('Y-m-d H:i:s');
+                $query = "INSERT INTO tienen (idPartida, idPregunta, fecha) VALUES (?,?, ?)";
+                $stmt = $this->db->connection->prepare($query);
+                $stmt->bind_param("iis", $idPartida, $idPregunta, $fecha);
+                $stmt->execute();}
+    private function registrarPreguntaEnUsuarioPregunta($idUsuario, $idPregunta){
+        $query = "INSERT INTO usuarioPregunta (idUsuario, idPregunta) VALUES (?,?)";
+        $stmt = $this->db->connection->prepare($query);
+        $stmt->bind_param("ii", $idUsuario, $idPregunta);
+        $stmt->execute();}
     public function traerPuntajeDelUsuarioEnLaPartida($idPartida, $idUsuario) {
         // Preparar la consulta SQL
         $query = "SELECT SUM(t.puntaje) AS total_puntaje
@@ -267,201 +304,12 @@ class PartidaModel
                   WHERE p.id = ? AND u.id = ?
                   GROUP BY u.id";
 
-        // Preparar la sentencia
         $stmt = $this->db->connection->prepare($query);
-
-        // Vincular los parámetros
         $stmt->bind_param("ii", $idPartida, $idUsuario);
-
-        // Ejecutar la consulta
         $stmt->execute();
-
-        // Obtener el resultado
         $resultado = $stmt->get_result()->fetch_assoc();
-
-
-
-        // Retornar el puntaje total o null si no se encuentra el registro
         return $resultado ? $resultado['total_puntaje'] : 0;
 
     }
 
-
-
-    public function getPregunta($idUsuario){
-        $preguntasNoVistasPorUsuario = $this->getPreguntasNoVistasPorUsuario($idUsuario);
-        if($preguntasNoVistasPorUsuario){
-
-
-        $idPregunta = $this->getPreguntaAleatoria($preguntasNoVistasPorUsuario);
-        $query = "SELECT p.id as idPregunta, p.descripcion as pregunta, r.descripcion as respuesta, r.id as idRta
-              FROM pregunta p 
-              JOIN respuesta r ON p.id = r.idPregunta 
-              WHERE p.id = ?";
-
-        $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("i", $idPregunta);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        $data = [
-            "pregunta" => [
-                "idPregunta" => null,
-                "descripcion" => ""
-            ],
-            "respuestas" => []
-        ];
-
-        while ($row = $result->fetch_assoc()) {
-            if ($data["pregunta"]["idPregunta"] === null) {
-                $data["pregunta"]["idPregunta"] = $row["idPregunta"];
-                $data["pregunta"]["descripcion"] = $row["pregunta"];
-            }
-
-            $data["respuestas"][] = [
-                "respuesta" => $row["respuesta"],
-                "idRta" => $row["idRta"]
-            ];
-        }
-        shuffle($data['respuestas']);
-        return $data;
-        }
-        return null;
-    }
-
-
-    public function validarRespuesta($idRespuesta){
-        $query = "SELECT * FROM respuesta WHERE id = ?";
-        $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("i", $idRespuesta);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        if($row = $result->fetch_assoc()){
-            if($row["esCorrecta"] == 1){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public function registrarPreguntaCorrecta($idPartida, $idPregunta){
-        $fechaHoraActual = new DateTime();
-        $fechaFormateada = $fechaHoraActual->format('Y-m-d H:i:s');
-        $query = "INSERT INTO tienen (idPartida, idPregunta,fecha,puntaje) VALUES (?,?,?,?)";
-        $puntaje = 1;
-        $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("iisi", $idPartida, $idPregunta,$fechaFormateada,$puntaje);
-        $stmt->execute();
-    }
-    public function registrarPreguntaIncorrecta($idPartida, $idPregunta){
-        $fechaHoraActual = new DateTime();
-        $fechaFormateada = $fechaHoraActual->format('Y-m-d H:i:s');
-        $query = "INSERT INTO tienen (idPartida, idPregunta,fecha,puntaje) VALUES (?,?,?,?)";
-        $puntaje = 0;
-        $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("iisi", $idPartida, $idPregunta,$fechaFormateada,$puntaje);
-        $stmt->execute();
-    }
-
-    private function getPreguntasNoVistasPorUsuario($idUsuario)
-    {
-        /*$dificultad['dificil'] = "WITH PreguntasDificiles AS (
-    SELECT tienen.idPregunta AS idPreguntaDificil,
-           AVG(tienen.puntaje) AS promedio
-    FROM tienen
-    GROUP BY tienen.idPregunta
-    HAVING promedio < 0.3
-)";
-        $dificultad['medio'] = "WITH PreguntasDificiles AS (
-    SELECT tienen.idPregunta AS idPreguntaDificil,
-           AVG(tienen.puntaje) AS promedio
-    FROM tienen
-    GROUP BY tienen.idPregunta
-    HAVING promedio > 0.3 and < 0.7
-)";
-        $dificultad['facil'] = "WITH PreguntasDificiles AS (
-    SELECT tienen.idPregunta AS idPreguntaDificil,
-           AVG(tienen.puntaje) AS promedio
-    FROM tienen
-    GROUP BY tienen.idPregunta
-    HAVING promedio > 0.7
-)";
-
-        $queryFuncional = "{$dificultad['dificil']} 
-SELECT pr.id AS idPreguntaNoVista
-FROM pregunta pr
-WHERE pr.id NOT IN (
-    SELECT up.idPregunta
-    FROM usuario u
-    JOIN UsuarioPregunta up ON u.id = up.idUsuario
-    WHERE u.id = ?
-)
-AND pr.id IN (
-    SELECT idPreguntaDificil
-    FROM PreguntasDificiles
-);";*//*
-            $queryFuncional = "SELECT pr.id AS idPreguntaNoVista
-                   FROM pregunta pr
-                   WHERE pr.id NOT IN (
-                       SELECT up.idPregunta as id
-                       FROM usuario u
-                       JOIN UsuarioPregunta up on u.id = up.idUsuario
-                       WHERE u.id = ?
-                       ORDER BY u.id
-                   );";
-        $stmt = $this->db->connection->prepare($queryFuncional);
-        $stmt->bind_param("i", $idUsuario);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $preguntasNoVistasPorUsuario = [];
-        while($row = $result->fetch_assoc()){
-            $preguntasNoVistasPorUsuario[] = $row["idPreguntaNoVista"];
-        }
-        return $preguntasNoVistasPorUsuario;
-    }
-
-    private function getPreguntaAleatoria(array $preguntasNoVistasPorUsuario){
-        $preguntas = $preguntasNoVistasPorUsuario;
-        $idPreguntaAleatoria = array_rand($preguntas);
-        return $preguntas[$idPreguntaAleatoria];
-    }
-    public function desactivarPartida($idPartida){
-        $query = "UPDATE partida SET estado = 'inactivo' WHERE id = ?";
-        $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("i",$idPartida);
-        $stmt->execute();
-    }
-
-    public function crearPartida($idUsuario){
-        $fecha = date('Y-m-d H:i:s');
-        $estado = "Activo";
-        $query = "INSERT INTO partida(fecha, estado,idUsuario) VALUES (?,?,?)";
-        $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("ssi", $fecha, $estado, $idUsuario);
-        $stmt->execute();
-    }
-
-    public function getPartida($idUsuario){
-        $query = "SELECT id FROM partida WHERE idUsuario = ? AND estado = 'activo'";
-        $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("i", $idUsuario);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        return $result->fetch_assoc();
-    }
-
-    public function reiniciarRegistroDePreguntasVistasPorUsuario($idUsuario){
-        $query = "DELETE FROM UsuarioPregunta WHERE idUsuario = ?";
-        $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("i", $idUsuario);
-        $stmt->execute();
-    }
-
-    public function marcarPreguntaVistaPorUsuario($idUsuario, $idPregunta){
-        $query = "INSERT INTO UsuarioPregunta (idUsuario, idPregunta) VALUES (?,?)";
-        $stmt = $this->db->connection->prepare($query);
-        $stmt->bind_param("ii", $idUsuario, $idPregunta);
-        $stmt->execute();
-    }
-}*/
 }
