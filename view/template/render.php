5bd541d11df2bfe7f2e233b7a155e632f73019e2
magali <maga.bjb@gmail.com>
2024-11-24 16:34:08 -0300

    mejore estilos

---
+++ view/template/render.php
@@ -0,0 +1,20 @@
+
+<html lang="es"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"></html>
+<?php
+
+
+
+require 'C:/xampp/htdocs/TP-Final-PW2/vendor/autoload.php'; // Asegúrate de que la ruta es correcta
+
+// Crea el motor de plantillas Mustache
+$mustache = new Mustache_Engine;
+
+// Carga la plantilla
+$template = file_get_contents('./dashboardView.mustache');
+
+
+
+// Renderiza la plantilla
+$output = $mustache->render($template);
+
+echo $output;
